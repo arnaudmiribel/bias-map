8158e67684728a92885080b34a072eee05664509
arnaud <arnaudmiribel@gmail.com>
2022-03-21 14:26:20 +0100

    Add examples

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,28 @@ def result_to_positive_class_probability(result):
 countries_json = get_countries_json()
 classifier = get_classifier()
 st.title("🗺️ Bias map")
-st.write("""Inspired by this [tweet](https://twitter.com/aureliengeron/status/1505402534407524353?s=21) from Aurélien Geron and 
-the code available in this [Colab](https://colab.research.google.com/gist/ageron/fb2f64fb145b4bc7c49efc97e5f114d3/biasmap.ipynb#scrollTo=ac6a454f)
-we let viewers create arbitrary such "bias maps" based on whether a given sentence turns out positive or 
-negative according to a deep learning sentiment analyser.
-""")
+st.caption("""Inspired by this [tweet](https://twitter.com/aureliengeron/status/1505402534407524353?s=21) from Aurélien Geron and 
+the code available in this [Colab](https://colab.research.google.com/gist/ageron/fb2f64fb145b4bc7c49efc97e5f114d3/biasmap.ipynb#scrollTo=ac6a454f)""")
+st.write("""Discover whether [DistilBert](https://huggingface.co/distilbert-base-uncased-finetuned-sst-2-english) is biased towards certain countries!
+Simply input a sentence which would countain a country name (using `*` as a country placeholder), and we will look at 
+how the model predictions vary for all possible countries.""")
+
 text_input = st.text_input(
-    label="Type in a sentence. Use * to specify country placeholder", value="This movie was filmed in *"
+    label="Type in a sentence. Use '*' as a country placeholder", value="This movie was filmed in *"
 )
+
+st.write("Or use other examples:")
+columns = st.columns(3)
+book = columns[0].button("The book is written in *")
+partner = columns[1].button("My partner is from *")
+candidate = columns[2].button("Our next candidate is from *")
+
+if book:
+    text_input = "The book is written in *"
+if partner:
+    text_input = "My partner is from *"
+if candidate:
+    text_input = "Our next candidate is from *"
 
 assert "*" in text_input, "Use the placeholder!"
 
@@ -60,7 +74,3 @@ if text_input:
     )
 
     st.plotly_chart(bias_map)
-    share = st.button("📷 Screenshot & share")
-    if share:
-        screenshot = bias_map.to_image(format="png")
-        st.image(screenshot)
