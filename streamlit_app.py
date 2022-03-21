caa2d1d409bc7501bdd8acaa7057e7c921b2bef0
arnaud <arnaudmiribel@gmail.com>
2022-03-21 14:37:19 +0100

    Add raw data

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,4 +84,4 @@ if text_input:
         st.plotly_chart(bias_map)
 
         st.write("All data (sorted by ascending 'positive'-ness probability)")
-        st.dataframe(countries_df.sort_values(by="Positive class probability", ascending=False), height=350,)
+        st.dataframe(countries_df.sort_values(by="Positive class probability", ascending=True), height=350,)
